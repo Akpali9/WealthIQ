16afbc4c43755e905c9d1bbd52c6fc67ac101511
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-05-22 14:32:07 +0100

    Create colors.dart

---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF00FF88);
+  static const background = Color(0xFF0B1F3A);
+  static const card = Color(0xFF122B4A);
+  static const text = Colors.white;
+  static const muted = Colors.grey;
+}
