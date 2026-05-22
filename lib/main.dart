d57bccac360019b1945cb8b3c8405a709611f91d
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-05-22 14:38:38 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'app.dart';
+import 'services/finance_provider.dart';
 
 void main() {
-  runApp(const WealthIQ());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => FinanceProvider(),
+      child: const WealthIQ(),
+    ),
+  );
 }
